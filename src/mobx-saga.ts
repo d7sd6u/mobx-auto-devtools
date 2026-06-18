c650901a450559c9bd167ef237341ef88dbc6852
d7sd6u <58745001+d7sd6u@users.noreply.github.com>
2026-06-18 15:29:31 +0400

    feat: return compliant initializer from @saga

--- src/mobx-saga.ts
+++ src/mobx-saga.ts
@@ -43,7 +43,7 @@ export function getOrigFunction(fn: UnknownFunction): Function | undefined {
 export function saga<This extends object, Args extends any[], Return>(
   target: undefined,
   context: ClassFieldDecoratorContext<This, (this: This, ...args: Args) => Return>,
-): () => (this: This, ...args: Args) => Return;
+): (this: This, initValue: (this: undefined, ...args: Args) => Return) => (this: This, ...args: Args) => Return;
 export function saga<This extends object, Args extends any[], Return>(
   target: (this: This, ...args: Args) => Return,
   context: ClassMethodDecoratorContext<This, (this: This, ...args: Args) => Return>,
